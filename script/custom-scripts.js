1be3623f89dfd905b4d8702ecb17860cb51da56d
bIropka <biropka@gmail.com>
2016-11-05 21:36:26 +0200

    Initial commit

---
+++ script/custom-scripts.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    /*******************************************************************************************************************
+     * window resize
+     */
+
+    if ($(window).width() < '992'){
+        
+    } else {
+
+    }
+
+    $(window).resize(function(){
+        if ($(window).width() < '992'){
+
+        } else {
+
+        }
+    });
+
+    /*******************************************************************************************************************
+     * buttons, clicks, hovers
+     */
+    
+
+    /*******************************************************************************************************************
+     * buttons, clicks, hovers
+     */
+
+    $('.btn-sign-up').click(function() {
+        $('.dropdown-sign-up').addClass('visible');
+    });
+
+    $('.btn-sign-in').click(function() {
+        $('.dropdown-sign-in').addClass('visible');
+    });
+
+    $('.btn-callback').click(function() {
+        $('.dropdown-callback').addClass('visible');
+        return false;
+    });
+
+    $('.close-button').click(function () {
+        $(this).parents('.large-popup').removeClass('visible');
+    });
+
+
+    /*******************************************************************************************************************
+     * custom select
+     */
+
+    $('.current-lang').click(function() {
+        $('.nav-lang').slideToggle(200);
+    });
+
+    $('.select-main').on('change', function() {
+        $(this).parents('.select-wrapper').find('.select-hidden').fadeIn();
+    });
+
+    /*******************************************************************************************************************
+     * function on page load
+     */
+
+    $(window).load(function(){
+        $(".be-loader").fadeOut("slow");
+    });
+
+
+});
